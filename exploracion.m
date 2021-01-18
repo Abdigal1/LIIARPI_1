2ec81a6b4884aa75f2580837722c0d1d0aa22217
Abdigal <abdigal.cb@gmail.com>
2021-01-18 13:49:29 -0500

    New method v1

--- exploracion.m
+++ exploracion.m
@@ -1,10 +1,24 @@
 close all;clear;
-a = imread('c1anemia-238.jpg');
-
+a = imread('c1anemia-237.jpg');
+b = rgb2hsv(a);
+[W, H, ~] = size(a);
 %Sin flash
-%mask = 144<=a(:,:,1) & 177>= a(:,:,1) & 149<=a(:,:,2) &191>=a(:,:,2)& 152<=a(:,:,3) & 183>=a(:,:,3);
-%Con flash
-mask = 145<=a(:,:,1) & 167>= a(:,:,1) & 182<=a(:,:,2) &222>=a(:,:,2)& 198<=a(:,:,3) & 235>=a(:,:,3);
+flag = true;
+mask = 144<=a(:,:,1) & 177>= a(:,:,1) & 149<=a(:,:,2) &191>=a(:,:,2)& 152<=a(:,:,3) & 183>=a(:,:,3);
 mask = imfill(mask,4, 'holes');
+if sum(sum(mask))>0.15*W*H
+    flag = false;
+    disp('Cuac')%aumenta un poco 166 206
+    mask = 145<=a(:,:,1) & 167>= a(:,:,1) & 182<=a(:,:,2) &222>=a(:,:,2)& 198<=a(:,:,3) & 235>=a(:,:,3);
+    mask = imfill(mask,4, 'holes');
+end
+if flag
+    mask = mask & 0.5<=b(:,:,1) & 0.55>=b(:,:,1);
+end
+se = strel('line',25,25);
+mask = imdilate(mask, se);
+mask = imfill(mask,4, 'holes');
+%Con flash
+
 figure, imshow(mask);
 figure, imshow([bitand(im2uint8(mask), a), a]);
