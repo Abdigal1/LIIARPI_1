52ccdbd8c37f198afadc0fbd28c6b58ad3177142
Abdigal <abdigal.cb@gmail.com>
2021-01-19 01:25:53 -0500

    Reconocimiento ojo ajustado

--- exploracion_2.m
+++ exploracion_2.m
@@ -1,5 +1,6 @@
 close all;clear;
-a = imread('c1anemia-129.jpg');%Falta 101 572
+a = imread('c1anemia-152.jpg');%Falta 101 subir 664
+%Caso extremo 676
 b = rgb2hsv(a);
 [W, H, ~] = size(a);
 %Sin flash
@@ -9,7 +10,7 @@ mask = 144<=a(:,:,1) & 177>= a(:,:,1) & 149<=a(:,:,2)...
 mask = imfill(mask,4, 'holes');
 if sum(sum(mask))>0.15*W*H
     flag = false;
-    disp('Cuac')%aumenta un poco 166 206
+    disp('Caso 2')%aumenta un poco 166 206
     mask = 145<=a(:,:,1) & 167>= a(:,:,1) & 182<=a(:,:,2)...
         &222>=a(:,:,2)& 198<=a(:,:,3) & 235>=a(:,:,3);
     mask = imfill(mask,4, 'holes');
@@ -24,3 +25,19 @@ mask = imfill(mask,4, 'holes');
 
 figure, imshow(mask);
 figure, imshow([bitand(im2uint8(mask), a), a]);
+
+
+cc = bwconncomp(mask,4);
+P=cc.NumObjects;
+defectdata = regionprops(cc, 'all');
+[~,idx]=sort([defectdata.Area]);
+defectdata=defectdata(idx);
+firstb = defectdata(end).BoundingBox;
+x = firstb(1);
+y = firstb(2);
+ww = firstb(3);
+hh = firstb(4);
+nx = max(0,floor(x-ww/2));
+ny = max(0,floor(y-hh/2));
+rb = [nx ny min(H-nx, 2*ww) min(W-ny, 2*hh)];
+figure, imshow(imcrop(a, rb))
