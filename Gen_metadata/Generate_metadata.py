0485872e4a359b69f25cceb650cc18b2e004cbfc
guillermo <gmorenoc@uni.pe>
2021-05-22 01:33:08 -0500

    SLIC_GAT_BASE

--- Gen_metadata/Generate_metadata.py
+++ Gen_metadata/Generate_metadata.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/grmc1999/Documentos/LIIARPI/hmg/LIIARPI_1/Utils')
+sys.path.append('D:\\Documentos\\LIIARPI\\Anemia\\GH\\LIIARPI_1\\Utils')
 from Utilities import *
 import numpy as np
 from skimage import io
@@ -9,7 +9,7 @@ from skimage.segmentation import slic
 from skimage import morphology
 import json
 
-f1=open("/home/grmc1999/Documentos/LIIARPI/hmg/Base_de_Datos/validcrop.txt","r")
+f1=open("D:\\Documentos\\LIIARPI\\Anemia\\Base_de_Datos\\validcrop.txt","r")
 lines=f1.readlines()
 linesn=np.array(lines)
 linesn=np.delete(lines,np.where(linesn=="\n"))
@@ -22,15 +22,18 @@ xywh=v_no_spaces(xywh)
 xywh=np.vectorize(pyfunc=(lambda x:float(x)))(xywh.reshape(1,-1)[0])
 xywh=xywh.reshape(-1,4).astype(int)+1
 
-dir_origin='/home/grmc1999/Documentos/LIIARPI/hmg/Base_de_Datos/Imagenes_Originales/'
-dir_ROI='/home/grmc1999/Documentos/LIIARPI/hmg/Base_de_Datos/Sem_Auto/eye_'
-dir_meta='/home/grmc1999/Documentos/LIIARPI/hmg/Base_de_Datos/Metadata/'
+dir_origin='D:\\Documentos\\LIIARPI\\Anemia\\Base_de_Datos\\Imagenes_Originales\\'
+dir_ROI='D:\\Documentos\\LIIARPI\\Anemia\\Base_de_Datos\\Sem_Auto\\eye_'
+dir_meta='D:\\Documentos\\LIIARPI\\Anemia\\Base_de_Datos\\Metadata\\'
 #FOR
 for name in imgnames:
-    img = io.imread(dir_origin+name)
-    ROI = io.imread(dir_ROI+name)
-    mask=assemble_mask(xywh[np.where(imgnames==(name))][0],img,ROI)
-    lum = np.mean(mask,axis=2).astype(int)
-    mask1=lum > 0
-    SD=get_Statistical_Descriptors(img,mask1,n_segments=20)
-    np.save(dir_meta+name.split('.')[0]+'.npy',SD)
+    try:
+        img = io.imread(dir_origin+name)
+        ROI = io.imread(dir_ROI+name)
+        mask=assemble_mask(xywh[np.where(imgnames==(name))][0],img,ROI)
+        lum = np.mean(mask,axis=2).astype(int)
+        mask1=lum > 0
+        SD=get_Statistical_Descriptors(img,mask1,n_segments=20)
+        np.save(dir_meta+name.split('.')[0]+'.npy',SD)
+    except:
+        print(name)
